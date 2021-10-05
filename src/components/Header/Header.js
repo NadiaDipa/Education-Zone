7c7dafce090c47fe29bf58d219ffa22307c35506
NadiaDipa <nadiaislamdipa1@gmail.com>
2021-10-05 15:55:52 +0600

    added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar} from 'react-bootstrap';
+import {Container, Nav, Navbar} from 'react-bootstrap';
 import './Header.css'
 import { Link } from 'react-router-dom';
 
@@ -8,23 +8,20 @@ const Header = () => {
     
     return (
         <div>
+
+             <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand to="/home">Navbar</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Link className="nav-link" to="/home">Home</Link>
+                            <Link className="nav-link" to="/about">About</Link>
+                            <Link className="nav-link" to="/services">Services</Link>
+                            <Link className="nav-link" to="/contact">Contact Us</Link>
+                        </Nav>
+                </Container>
+            </Navbar>
             
-        <Navbar bg="dark" variant="dark">
-        <ul>
-            <Link to="/home">
-                <li>Home</li>
-            </Link>
-             <Link to ="/about">
-                <li>About</li>
-            </Link>
-             <Link to="/services">
-                <li>Services</li>
-            </Link>
-             <Link to="/contact">
-                <li>Contact Us</li>
-            </Link>
-        </ul>
-        </Navbar>
+       
 
         </div>
     );
